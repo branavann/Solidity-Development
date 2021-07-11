6dd1b5450aea41c7c99124205a3932fa58ed75de
Branavan Nagarajan <branavannagarajan@gmail.com>
2021-07-11 12:45:10 -0400

    triggerDelivery test

--- Project #2 - Supply Chain/test/ItemManager.js
+++ Project #2 - Supply Chain/test/ItemManager.js
@@ -23,7 +23,7 @@ contract("ItemManager Test", async accounts => {
     assert.equal(itemInformation._price, itemPrice, "The price of the item is wrong");
     assert.equal(result.logs[0].args._itemIndex, 0, "It's not the first item in the list");
   })
-  
+
   it("Updates the _state variable after recieving payment for the item", async () => {
     
     // Parameters for our example item
@@ -44,6 +44,30 @@ contract("ItemManager Test", async accounts => {
 
     // Checking if the value for _state updated from 0 (creation) to 1 (payment)
     assert.equal(itemInformation._state, 1, "The _state counter has not incremented");
+
+  })
+
+  it("Updates the _state variable after recieving delivering the item", async () => {
+    
+    // Parameters for our example item
+    const itemName = "Example";
+    const itemPrice = 100;
+
+    // Creating an instance of the ItemManager contract
+    const instance = await ItemManager.deployed();
+    const result = await instance.createItem(itemName, itemPrice, {from: accounts[0]});
+
+    // Payment for the item
+    const itemAddress = result.logs[0].args._itemAddress;
+    const payment = await web3.eth.sendTransaction({from: accounts[1], to: itemAddress, value: 100});
+    
+    // Delivery of the item
+    const index = result.logs[0].args._itemIndex;
+    const delivery = await instance.triggerDelivery(index);
+    const itemInformation = await instance.items(index, {from: accounts[0]});
+
+    // Checking if the value for _state updated from 0 (creation) to 1 (payment)
+    assert.equal(itemInformation._state, 2, "The _state counter has not incremented");
 
   })
 });
