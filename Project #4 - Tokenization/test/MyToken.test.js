9e0d804f8d3b226f431c0812378986bf3eb8b8c7
Branavan Nagarajan <branavannagarajan@gmail.com>
2021-07-15 10:41:07 -0400

    AssertionError for Truffle test. Revisit and revise the tests.

--- Project #4 - Tokenization/test/MyToken.test.js
+++ Project #4 - Tokenization/test/MyToken.test.js
@@ -15,9 +15,49 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 contract("BKN Token Test", async (accounts) => {
+
+    // Global variables 
+    const [ initialHolder, recipient, anotherAccount ] = accounts;
+
     it("Deposits 100 BKN token within the Owner's address", async () => {
-        const instance = await MyToken.deployed();
-        const balance = await instance.totalSupply();
-        assert(balance, 100, "Incorrect number of tokens were deposited");
+        
+        let instance = await MyToken.new(100);
+        let _totalSupply = await instance.totalSupply();
+
+        const initialHolderBalance = await instance.balanceOf(initialHolder);
+        assert(initialHolderBalance, _totalSupply , "Incorrect number of tokens were deposited");
+    });
+
+    it("Transfers the correct number of BKN tokens", async () => {
+
+        let instance = await MyToken.new(100);
+        let _totalSupply = await instance.totalSupply();
+
+        // Specifying the amount to transfer
+        const transferAmount = 10;
+ 
+        // Checking if the transaction was successful
+        expect(instance.transfer(recipient, transferAmount)).to.eventually.be.fulfilled;
+
+        // Checking account balances after the transaction
+        expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(_totalSupply.sub(new BN(transferAmount)));
+        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(transferAmount));
+
+    });
+
+    it("Prevents transferring more tokens than the amount available in an account", async () => {
+
+        let instance = await MyToken.new(100);
+        let _totalSupply = await instance.totalSupply();
+
+        // Checking the initial balance of the initialHolder account
+        console.log(await instance.balanceOf(initialHolder));
+        //const initialHolderBalance = await instance.balanceOf(initialHolder);
+        //const transferAmount = initialHolderBalance + 10;
+
+        // Checking if the trasnaction failed
+        // expect(instance.transfer(recipient, transferAmount)).to.eventually.be.rejected;
+
+
     });
 });
