e739af266161f748d0eb0cbccc548319c4284684
Branavan Nagarajan <branavannagarajan@gmail.com>
2021-07-26 10:36:47 -0400

    Changed solidity complier to address migration issues

--- Project #4 - Tokenization/contracts/MyToken.sol
+++ Project #4 - Tokenization/contracts/MyToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Branavan's Token", "BKN") {
+    constructor(uint256 initialSupply) ERC20("Branavan's Token", "BKN") public {
         _mint(msg.sender, initialSupply);
     }
 }
--- Project #4 - Tokenization/migrations/2_deploy_contract.js
+++ Project #4 - Tokenization/migrations/2_deploy_contract.js
@@ -1,5 +1,12 @@
 var MyToken = artifacts.require("./MyToken.sol");
+var MyTokenSale = artifacts.require("./MyTokenSale.sol");
 
 module.exports = async function(deployer) {
+    let _address = await web3.eth.getAccounts();
     await deployer.deploy(MyToken, 100);
+    await deployer.deploy(MyTokenSale, 1, _address[0], MyToken.address); // address for MyToken is stored within the artifacts 
+    let instance = await MyToken.deployed();
+    // Transfers the tokens to the MyTokenSale address
+    await instance.transfer(MyTokenSale.address, 100);
+
 }
